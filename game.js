63597f433bc77e14e20e3538c108cf7344dea261
Ishan Varshney <ishanvarshney99@gmail.com>
2019-02-18 18:00:52 +0530

    Added functionality to rest of the buttons

--- game.js
+++ game.js
@@ -1,41 +1,67 @@
 new Vue({
-	el:"#app",
+	el:"#app",                               
 	data:{
 		show:true,
 		you:100,                  
 		monster:100,
+		widthY:350,
+		widthM:350,
 		moveY:0,
-		moveM:0
+		moveM:0,
+		timerId:null
 	},
 	methods:{
 		resetGame:function()
 		{ 
 			this.monster=100;
 			this.you=100;
+            this.widthY=350,
+            this.widthM=350,
 			this.moveY=0;
-			this.moveM=0;
+			clearTimeout(this.timerId);
+			this.disableButtons(false);
+			this.moveM=0;   
 		},
-		update:function(){
-			var damageM=this.calculateDamage(3,12);
+		update:function(event)
+		{
+			console.log("UPDATE METHOD");
+			var damageM=0;
+            if(event.target.id==="attack")
+			   damageM=this.calculateDamage(3,12);
+		    else if(event.target.id==="specialAttack")
+               damageM=this.calculateDamage(12,21);
+            else if(event.target.id==="heal"){
+            	var limit=100-this.you;
+            	this.you+=Math.trunc(limit/2);
+            	this.widthY=(this.you*3.5)
+        	    this.moveY=(350-(this.you*3.5))/2;
+            }
+            else if(event.target.id==="giveUp"){
+            	this.show=true;
+            	this.resetGame();
+            	return;
+            }
          this.monster-=damageM;
+         this.widthM=(this.monster*3.5);
          this.moveM=(350-(this.monster*3.5))/2;
          var q=this;
-         setTimeout(function(){
+         this.timerId=setTimeout(function(){
          	q.monsterAttack();
-         },1000);  
-         if(this.checkWin())
-				return;
-         },
+         	q.disableButtons(false)
+         },2000);  
+         this.disableButtons(true)
+         this.checkWin()
+     },
         monsterAttack:function()
         {
-        	if(this.checkWin())
-        		return;
         	var damageY=this.calculateDamage(3,12);
         	 this.you-=damageY;
-        	 this.moveY=(350-(this.you*3.5))/2;         
+        	 this.widthY=(this.you*3.5)
+        	 this.moveY=(350-(this.you*3.5))/2; 
+        	 this.checkWin()       
         },    
 		calculateDamage:function(min,max)
-		{
+		{   
 			return Math.floor((Math.random() * max) + min);
 		},
         checkWin:function()
@@ -43,14 +69,18 @@ new Vue({
         	if(this.you<=0){
         		this.resetGame();
         		alert('You Lose!!');
-                return true;
         	}
-        	else if(this.monster<=0){
+        	else if(this.monster<=0){   
         		this.resetGame();
         		alert('You Win!!');
-        		return true;
         	}
-        	return false;
-        }
+        },
+        disableButtons:function(bool){
+        	var buttons=document.getElementsByTagName("button");
+        	    for(var i=0;i<buttons.length;i++){
+        	    	var button=buttons[i];
+        	    		button.disabled=bool;
+        	    }
+        	}
 	}
 });
